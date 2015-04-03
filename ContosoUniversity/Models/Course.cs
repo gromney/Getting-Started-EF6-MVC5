9e59ab93fb2288287c18cade2ab84dc4865eb310
Geronimo Romney <g.romney@gmail.com>
2015-04-03 14:35:36 -0400

    Adding dataanotation to Course model

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -7,5 +7,10 @@ namespace ContosoUniversity.Models
 {
     public class Course
     {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Rnrollments { get; set; }
     }
 }
