c93940bcc1dad40756ff4654776ff89c0f72a0ff
Geronimo Romney <g.romney@gmail.com>
2015-04-03 14:11:09 -0400

    Create the Data Model

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollementDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
