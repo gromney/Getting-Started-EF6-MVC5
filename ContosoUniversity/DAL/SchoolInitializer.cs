06df617c19b809046b49568a46bfe4aa7470e020
Geronimo Romney <g.romney@gmail.com>
2015-04-05 21:12:05 -0400

    Seed the Database with Test Data

--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -27,13 +27,13 @@ namespace ContosoUniversity.DAL
             context.SaveChanges();
             var courses = new List<Course>
             {
-            new Course{CourseId=1050,Title="Chemistry",Credits=3,},
-            new Course{CourseId=4022,Title="Microeconomics",Credits=3,},
-            new Course{CourseId=4041,Title="Macroeconomics",Credits=3,},
-            new Course{CourseId=1045,Title="Calculus",Credits=4,},
-            new Course{CourseId=3141,Title="Trigonometry",Credits=4,},
-            new Course{CourseId=2021,Title="Composition",Credits=3,},
-            new Course{CourseId=2042,Title="Literature",Credits=4,}
+            new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+            new Course{CourseID=4022,Title="Microeconomics",Credits=3,},
+            new Course{CourseID=4041,Title="Macroeconomics",Credits=3,},
+            new Course{CourseID=1045,Title="Calculus",Credits=4,},
+            new Course{CourseID=3141,Title="Trigonometry",Credits=4,},
+            new Course{CourseID=2021,Title="Composition",Credits=3,},
+            new Course{CourseID=2042,Title="Literature",Credits=4,}
             };
             courses.ForEach(s => context.Courses.Add(s));
             context.SaveChanges();
